a424e14109917e707c5ec3a3eaeb88440dd5cd52
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-11-12 15:27:12 +0530

    Recipes and Recipe Information JSON responses completed and working

--- models/recipeModel.go
+++ models/recipeModel.go
@@ -1,36 +1,64 @@
 package models
 
-// type Ingredient struct {
-// 	ID     int      `json:"id"`
-// 	Amount int      `json:"amount"`
-// 	Unit   string   `json:"unit"`
-// 	Name   string   `json:"name"`
-// 	Meta   []string `json:"meta"`
-// 	Image  string   `json:"image"`
-// }
-
-//type Recipe struct {
-//	ID                int          `json:"id"`
-//	Title             string       `json:"title"`
-//	Image             string       `json:"image"`
-// MissedIngredients []Ingredient `json:"missedIngredients"`
-// UsedIngredients   []Ingredient `json:"usedIngredients"`
-// UnusedIngredients []Ingredient `json:"unusedIngredients"`
-//}
-
-// type Recipes struct {
-// 	Results []Recipe
-// }
-
 type Recipe struct {
-	ID				int				`json:"id"`
-	Title			string			`json:"title"`
-	Image			string			`json:"image"`
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Image string `json:"image"`
 }
 
+// type Ingredient []struct {
+// 	ID				int				`json:"id"`
+// 	Name			string			`json:"name"`
+// 	Amount			int				`json:"amount"`
+// 	Unit			string			`json:"unit"`
+// }
+
+// type RecipeInformation struct {
+// 	Vegetarian		bool			`json:"vegetarian"`
+// 	Vegan			bool			`json:"vegan"`
+// 	GlutenFree		bool			`json:"glutenFree"`
+// 	DairyFree		bool			`json:"dairyFree"`
+// 	VeryHealthy		bool			`json:"veryHealthy"`
+// 	ID				int				`json:"id"`
+// 	Title			string			`json:"title"`
+// 	SourceURL		string			`json:"sourceUrl"`
+// 	ExtendedIngredients		[]interface{}	`json:"ingredients"`
+// }
+
 type RecipeInformation struct {
-	Vegetarian		bool			`json:"vegetarian"`
-	ID				int				`json:"id"`
-	Title			string			`json:"title"`
-	SourceURL		string			`json:"sourceUrl"`
+	Vegetarian           bool                   `json:"vegetarian"`
+	Vegan                bool                   `json:"vegan"`
+	GlutenFree           bool                   `json:"glutenFree"`
+	DairyFree            bool                   `json:"dairyFree"`
+	VeryHealthy          bool                   `json:"veryHealthy"`
+	Cheap                bool                   `json:"cheap"`
+	VeryPopular          bool                   `json:"veryPopular"`
+	Sustainable          bool                   `json:"sustainable"`
+	LowFodmap            bool                   `json:"lowFodmap"`
+	ExtendedIngredients  []ExtendedIngredients  `json:"extendedIngredients"`
+	ID                   int                    `json:"id"`
+	Title                string                 `json:"title"`
+	ReadyInMinutes       int                    `json:"readyInMinutes"`
+	Servings             int                    `json:"servings"`
+	SourceURL            string                 `json:"sourceUrl"`
+	SpoonacularSourceURL string                 `json:"spoonacularSourceUrl"`
+	Image                string                 `json:"image"`
+	Summary              string                 `json:"summary"`
+	Instructions         string                 `json:"instructions"`
+	AnalyzedInstructions []AnalyzedInstructions `json:"analyzedInstructions"`
+}
+
+type ExtendedIngredients struct {
+	ID     int     `json:"id"`
+	Name   string  `json:"name"`
+	Amount float64 `json:"amount"`
+	Unit   string  `json:"unit"`
+}
+
+type Steps struct {
+	Number int    `json:"number"`
+	Step   string `json:"step"`
+}
+type AnalyzedInstructions struct {
+	Steps []Steps `json:"steps"`
 }
